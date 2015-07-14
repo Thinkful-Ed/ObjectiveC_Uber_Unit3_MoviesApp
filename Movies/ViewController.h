11d1f848507e68ab9d5d89f2adb94abdb2b2f79c
craiggrummitt <craiggrummitt@gmail.com>
2015-07-14 14:51:05 -0500

    Initial Commit

---
+++ Movies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Movies
+//
+//  Created by Craig on 14/07/2015.
+//  Copyright (c) 2015 Thinkful. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

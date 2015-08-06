3c42f235787e2ef5550426059cbd8f77ca709d7b
craiggrummitt <craiggrummitt@gmail.com>
2015-08-06 12:46:53 -0500

    Added model

---
+++ Movies/Movie.m
@@ -0,0 +1,18 @@
+//
+//  Movie.m
+//  Movies
+//
+//  Created by Craig on 6/08/2015.
+//  Copyright (c) 2015 Thinkful. All rights reserved.
+//
+
+#import "Movie.h"
+
+@implementation Movie
+
+//init Note - coordinate defaults to 0,0 initially
+-(void)searchMovie:(NSString *)movie {
+    
+}
+
+@end
